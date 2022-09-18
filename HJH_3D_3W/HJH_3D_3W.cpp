4d92934e57004a54ce3bc88396ad5be4f1c37ae1
재현 한 <0501wogus@naver.com>
2022-09-18 20:30:09 +0900

    사인과 코사인을 이용해 반지름이 1인 원을 만들어 보았습니다.

--- HJH_3D_3W/HJH_3D_3W.cpp
+++ HJH_3D_3W/HJH_3D_3W.cpp
@@ -13,7 +13,7 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 
-}
+}        
 
 
 int main(void)
